15b8870889fb865258969613db5d5fb15e26c125
Tristan Leonard <tristanl@slalom.com>
2019-04-16 10:00:33 -0700

    Properly handling bad configuration and response.

--- handler.ts
+++ handler.ts
@@ -13,16 +13,29 @@ const handler: Handler = async (event: any, context: Context, callback: Callback
   const authTokenService = new AuthTokenService(environment)
 
   console.log("Generating Token")
-  const response = await authTokenService.generateToken()
-  const token = response.data['access_token']
-  const status = response.status
-  return { 
-    statusCode: status, 
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify({authToken: token})
+  try {
+    const response = await authTokenService.generateToken()
+    const token = response.data['access_token']
+    const status = response.status
+    return { 
+      statusCode: status, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({authToken: token})
+    }
+  } catch (error) {
+    const response = error.response;
+    const responseText = response.status == 401 ? "An unauthorized status was configured. Check that your environment variables have been properly defined." : response.statusText
+    return { 
+      statusCode: response.status, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({error: responseText})
+    }
   }
 };
 
